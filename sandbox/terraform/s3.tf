de3c12797ab4478d14b41bcb37d8d069764e5ee9
Shubhangi <sasadare@amazon.com>
2024-08-11 15:46:11 +0000

    'adding vpc'

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -1,6 +1,14 @@
-data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
 
-resource "aws_s3_bucket" "sandbox_bucket" {
-  bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
-  acl    = "private"
+
+# data "aws_caller_identity" "current" {}
+
+# resource "aws_s3_bucket" "sandbox_bucket" {
+#   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
+#   acl    = "private"
+# }
+
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }
