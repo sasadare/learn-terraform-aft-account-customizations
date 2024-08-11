b1f0888b91675995d718ce7de931210513d0f511
Shubhangi <sasadare@amazon.com>
2024-08-11 20:33:33 +0000

    'account customization'

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -1,14 +1,14 @@
-data "aws_region" "current" {}
+# data "aws_region" "current" {}
 
 
-# data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "current" {}
 
-# resource "aws_s3_bucket" "sandbox_bucket" {
-#   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
-#   acl    = "private"
-# }
-
-
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_s3_bucket" "sandbox_bucket" {
+  bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
+  acl    = "private"
 }
+
+
+# resource "aws_vpc" "main" {
+#   cidr_block = "10.0.0.0/16"
+# }
