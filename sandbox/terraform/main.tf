bc5687e06d92247e7c2f3ee39baf85ab126a9bfc
Shubhangi <sasadare@amazon.com>
2024-08-21 16:04:17 +0000

    cust

--- sandbox/terraform/main.tf
+++ sandbox/terraform/main.tf
@@ -1,7 +1,7 @@
 # data "aws_region" "current" {}
 
 
-data "aws_caller_identity" "current" {}
+# data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "sandbox_bucket01" {
   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
