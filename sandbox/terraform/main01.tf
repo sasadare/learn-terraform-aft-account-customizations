a6e5d5045fe21f0ae5cf4de341e3121e72e2ba6a
Shubhangi <sasadare@amazon.com>
2024-08-22 03:52:07 +0000

    buc creation

---
+++ sandbox/terraform/main01.tf
@@ -0,0 +1,9 @@
+# data "aws_region" "current" {}
+
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket" "sandbox_bucket" {
+  bucket = "aft-sandbox-ab-${data.aws_caller_identity.current.account_id}"
+  acl    = "private"
+}
