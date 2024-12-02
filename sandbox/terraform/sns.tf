d284c5bfd224e2abb61d9bdf6fe39be985464638
Shubhangi <sasadare@amazon.com>
2024-12-02 14:06:49 +0000

    Adding sns

--- sandbox/terraform/sns.tf
+++ sandbox/terraform/sns.tf
@@ -2,8 +2,8 @@ resource "aws_sns_topic" "sns-us-east-1" {
 name = "my-topic-name"
 }
 
-resource "aws_sns_topic" "sns-us-west-2" {
-name = "my-topic-name-1"
-provider = aws.second
+# resource "aws_sns_topic" "sns-us-west-2" {
+# name = "my-topic-name-1"
+# provider = aws.second
 
-}
+# }
